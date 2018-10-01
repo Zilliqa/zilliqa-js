f97f9a7f72edd85dcea11375e1a53a1eb62ba244
Ian Tan <iantanwx@gmail.com>
2018-10-01 18:24:04 +0800

    feat(): add main zilliqa-js package

---
+++ packages/zilliqa-js/src/index.ts
@@ -0,0 +1,34 @@
+import {HTTPProvider, Provider} from 'zilliqa-js-core';
+import {Wallet} from 'zilliqa-js-account';
+import {Contracts} from 'zilliqa-js-contract';
+import {Blockchain, Network} from 'zilliqa-js-blockchain';
+
+export default class Zilliqa {
+  static provider: Provider;
+  static setProvider(provider: Provider) {
+    Zilliqa.setProvider(provider);
+  }
+
+  provider: Provider;
+
+  blockchain: Blockchain;
+  contracts: Contracts;
+  network: Network;
+  wallet: Wallet;
+
+  constructor(node: string, provider?: Provider) {
+    // TODO: we need to perform duck typing here.
+    if (provider) {
+      Zilliqa.setProvider(provider);
+      this.provider = provider;
+    } else {
+      this.provider = new HTTPProvider(node);
+      Zilliqa.setProvider(this.provider);
+    }
+
+    this.blockchain = new Blockchain(this.provider);
+    this.contracts = new Contracts(this.provider);
+    this.network = new Network(this.provider);
+    this.wallet = new Wallet([]);
+  }
+}
