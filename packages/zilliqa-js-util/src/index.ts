4234af23fc7e807d7bc7d634ec804a49c05177ef
Ian Tan <iantanwx@gmail.com>
2018-10-22 17:18:03 +0800

    feat(util): add validation functions

--- packages/zilliqa-js-util/src/index.ts
+++ packages/zilliqa-js-util/src/index.ts
@@ -1,4 +1,5 @@
 import * as bytes from './bytes';
 import * as types from './types';
-export {bytes};
-export {types};
+import * as validation from './validation';
+
+export {bytes, types, validation};
