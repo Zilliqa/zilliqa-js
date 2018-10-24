94bfe11e0033cdd876c3c495baf433491e91d60d
Ian Tan <ian.tan@techinasia.com>
2018-10-24 15:49:17 +0800

    test(integration): fix imports

--- packages/zilliqa-js-blockchain/test/chain.ispec.ts
+++ packages/zilliqa-js-blockchain/test/chain.ispec.ts
@@ -1,6 +1,6 @@
 import BN from 'bn.js';
-import {HTTPProvider} from 'zilliqa-js-core';
-import {Account, Transaction, Wallet} from 'zilliqa-js-account';
+import { HTTPProvider } from '@zilliqa/zilliqa-js-core';
+import { Account, Transaction, Wallet } from '@zilliqa/zilliqa-js-account';
 import Blockchain from '../src/chain';
 
 jest.setTimeout(90000);
@@ -8,7 +8,6 @@ jest.setTimeout(90000);
 const accounts = [new Account(process.env.GENESIS_PRIV_KEY as string)];
 const provider = new HTTPProvider(process.env.HTTP_PROVIDER as string);
 const bc = new Blockchain(provider, new Wallet(provider, accounts));
-Transaction.setProvider(provider);
 
 describe('[Integration]: Blockchain', () => {
   it('should be able to get the latest DS block', async () => {
@@ -18,16 +17,16 @@ describe('[Integration]: Blockchain', () => {
   it('should be able to send a transaction', async () => {
     const transaction = new Transaction({
       version: 0,
-      to: '8254b2c9acdf181d5d6796d63320fbb20d4edd12',
+      toAddr: '8254b2c9acdf181d5d6796d63320fbb20d4edd12',
       amount: new BN(1000),
-      gasPrice: 1000,
-      gasLimit: 1000,
-    });
+      gasPrice: new BN(1000),
+      gasLimit: new BN(1000),
+    }, provider);
 
     const {
-      txParams: {receipt},
+      txParams: { receipt },
     } = await bc.createTransaction(transaction);
 
-    expect(receipt && receipt.success).toBeTruthy;
+    expect(receipt && receipt.success).toBeTruthy();
   });
 });
