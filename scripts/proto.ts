cae7bca9286ee449df834775607f98b9edb25e9b
Ian Tan <iantanwx@gmail.com>
2018-11-01 15:34:18 +0800

    fix(): create includes directory if not present

--- scripts/proto.ts
+++ scripts/proto.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
+import mkdirp from 'mkdirp';
 import pbjs from 'protobufjs/cli/pbjs';
 import pbts from 'protobufjs/cli/pbts';
 
@@ -23,32 +24,46 @@ const protoFiles = project.packages.reduce((files, { src }) => {
   return files;
 }, []);
 
-const compilation = new Promise((resolve, reject) => {
-  pbjs.main(
-    [
-      '--target',
-      'static-module',
-      '--wrap',
-      'commonjs',
-      '--out',
-      compiled,
-      ...protoFiles,
-    ],
-    (err, res) => {
-      if (err) {
-        return reject(err);
-      }
+const createOutDir = () => {
+  if (!fs.existsSync(proto)) {
+    mkdirp.sync(proto);
+  }
+};
 
-      resolve(res);
-    },
-  );
-})
-  .then(() => {
+const compile = async () => {
+  try {
+    createOutDir();
+
+    await new Promise((resolve, reject) => {
+      pbjs.main(
+        [
+          '--target',
+          'static-module',
+          '--wrap',
+          'commonjs',
+          '--out',
+          compiled,
+          ...protoFiles,
+        ],
+        (err, res) => {
+          if (err) {
+            return reject(err);
+          }
+
+          resolve(res);
+        },
+      );
+    });
+
     pbts.main(['--out', declaration, '--name', 'zproto', compiled], (err) => {
       if (err) {
-        log(`Could not generate protobuf files: ${err}`);
         throw err;
       }
     });
-  })
-  .catch((err) => process.exit(1));
+  } catch (err) {
+    log(`Could not generate protobuf files: ${err}`);
+    process.exit(1);
+  }
+};
+
+compile();
