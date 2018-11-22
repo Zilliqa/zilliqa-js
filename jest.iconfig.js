c60101cbffb27451eafe0b0354422000620a36c0
Ian Tan <iantanwx@gmail.com>
2018-11-22 10:50:51 +0800

    test(): fix proto build

--- jest.iconfig.js
+++ jest.iconfig.js
@@ -3,6 +3,7 @@ module.exports = {
   ...baseConfig,
   moduleNameMapper: {
     '^@zilliqa-js/zilliqa': '<rootDir>/packages/zilliqa/src/index.ts',
+    '^@zilliqa-js/proto$': '<rootDir>/packages/zilliqa-js-proto/dist/index.js',
     '^@zilliqa-js/(.*)$': '<rootDir>/packages/zilliqa-js-$1/src/index.ts',
   },
   setupTestFrameworkScriptFile: './jest-framework-setup.js',
