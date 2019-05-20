ac646e30eee6392159a13a5f86c7bb4602bd2224
Ian Tan <iantanwx@gmail.com>
2019-05-20 13:15:22 +0800

    feat(crypto): support basic bech32 encode/decode

---
+++ packages/zilliqa-js-crypto/test/bech32.spec.ts
@@ -0,0 +1,34 @@
+import { decode } from '../src/bech32';
+
+const VALID_CHECKSUM = [
+  'A12UEL5L',
+  'an83characterlonghumanreadablepartthatcontainsthenumber1andtheexcludedcharactersbio1tt5tgs',
+  'abcdef1qpzry9x8gf2tvdw0s3jn54khce6mua7lmqqqxw',
+  '11qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqc8247j',
+  'split1checkupstagehandshakeupstreamerranterredcaperred2y9e3w',
+];
+
+const INVALID_CHECKSUM = [
+  ' 1nwldj5',
+  '\x7F' + '1axkwrx',
+  'an84characterslonghumanreadablepartthatcontainsthenumber1andtheexcludedcharactersbio1569pvx',
+  'pzry9x0s0muk',
+  '1pzry9x0s0muk',
+  'x1b4n0q5v',
+  'li1dgmt3',
+  'de1lg7wt' + '\xFF',
+];
+
+describe('bech32 encoding', () => {
+  it('should decode valid checksums', () => {
+    VALID_CHECKSUM.forEach((encoded) => {
+      expect(!!decode(encoded)).toBeTruthy();
+    });
+  });
+
+  it('should not decode invalid checksums', () => {
+    INVALID_CHECKSUM.forEach((encoded) => {
+      expect(decode(encoded)).toBe(null);
+    });
+  });
+});
