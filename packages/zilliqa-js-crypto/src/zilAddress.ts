131375c3a8c347eb172c5e99af53f3c37d86f4e2
neeboo <neeboo@firestack.one>
2019-05-31 13:03:12 +0800

    fix(zilAddress)

--- packages/zilliqa-js-crypto/src/zilAddress.ts
+++ packages/zilliqa-js-crypto/src/zilAddress.ts
@@ -19,26 +19,53 @@ enum AddressType {
 }
 
 class ZilAddress {
+  static isValidBech32(address: string) {
+    return new ZilAddress(address).isBech32();
+  }
+
+  static isValidBase58(address: string) {
+    return new ZilAddress(address).isBase58();
+  }
+
+  static isValidChecksum(address: string) {
+    return new ZilAddress(address).isChecksum();
+  }
+
+  static isValidByte20(address: string) {
+    return new ZilAddress(address).isBytes20();
+  }
+
+  static isValidByte20Hex(address: string) {
+    return new ZilAddress(address).isBytes20Hex();
+  }
+
   raw: string;
 
   addressType?: AddressType;
 
-  bytes20Hex?: string;
-
   bytes20: string;
 
-  checkSum?: string;
+  get bytes20Hex(): string {
+    return `0x${this.bytes20}`;
+  }
 
-  bech32?: string;
+  get checkSum(): string {
+    return toChecksumAddress(this.bytes20);
+  }
 
-  base58?: string;
+  get bech32(): string {
+    return toBech32Address(this.bytes20);
+  }
+
+  get base58(): string {
+    return encodeBase58(this.bytes20);
+  }
 
   constructor(raw: string) {
     this.raw = raw;
     this.bytes20 = '0x';
     this.getAddressType();
   }
-
   isBytes20() {
     return this.addressType === AddressType.bytes20;
   }
@@ -66,37 +93,29 @@ class ZilAddress {
       this.bytes20 = this.raw.startsWith('0x')
         ? this.raw.substring(2)
         : this.raw;
-      this.normalize();
-    } else if (addrBool === true && checksumBool === true) {
+      return;
+    }
+    if (addrBool === true && checksumBool === true) {
       this.addressType = AddressType.checkSum;
       this.bytes20 = this.raw.toLowerCase().substring(2);
-      this.normalize();
-    } else if (
+      return;
+    }
+    if (
       bech32Bool === true &&
       validation.isAddress(fromBech32Address(this.raw))
     ) {
       this.addressType = AddressType.bech32;
       const decoded = fromBech32Address(this.raw).toLowerCase();
       this.bytes20 = decoded.startsWith('0x') ? decoded.substring(2) : decoded;
-      this.normalize();
-    } else if (
-      base58Bool === true &&
-      validation.isAddress(decodeBase58(this.raw))
-    ) {
+      return;
+    }
+    if (base58Bool === true && validation.isAddress(decodeBase58(this.raw))) {
       this.addressType = AddressType.base58;
       const decoded = decodeBase58(this.raw).toLowerCase();
       this.bytes20 = decoded.startsWith('0x') ? decoded.substring(2) : decoded;
-      this.normalize();
-    } else {
-      throw new Error('unknown address');
+      return;
     }
-  }
-
-  private normalize() {
-    this.bytes20Hex = `0x${this.bytes20}`;
-    this.checkSum = toChecksumAddress(this.bytes20);
-    this.base58 = encodeBase58(this.bytes20);
-    this.bech32 = toBech32Address(this.bytes20);
+    throw new Error('unknown address');
   }
 }
 
