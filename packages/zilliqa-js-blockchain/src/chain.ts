c2508665fcd5d4b790df77399847286ea9197e4f
Edison Lim <edison.limjh@gmail.com>
2019-10-14 06:57:01 +0800

    docs: added documentations for additional methods

--- packages/zilliqa-js-blockchain/src/chain.ts
+++ packages/zilliqa-js-blockchain/src/chain.ts
@@ -401,10 +401,10 @@ export class Blockchain implements ZilliqaModule {
   }
 
   /**
-   * getSmartContractCode
+   * getSmartContractCode - returns the smart contract code of a deployed contract.
    *
    * @param {string} address
-   * @returns {Promise<RPCResponse< code: string }, string>>}
+   * @returns {Promise<RPCResponse<{code: string }, string>>}
    */
   getSmartContractCode(
     addr: string,
@@ -431,7 +431,7 @@ export class Blockchain implements ZilliqaModule {
   }
 
   /**
-   * getSmartContractState
+   * getSmartContractState - retrieves the entire state of a smart contract
    *
    * @param {string} address
    * @returns {Promise<RPCResponse<any, string>>}
@@ -445,13 +445,17 @@ export class Blockchain implements ZilliqaModule {
   }
 
   /**
-   * getSmartContractSubState
+   * getSmartContractSubState - Queries the contract state, filtered by the variable names.
+   * This function is the filtered version of `getSmartContractState`.
+   * As `getSubState` performs the filtering, `variableName` of a field is required.
+   * If the `subState` is not found, this returns a `null` response.
    *
    * @param {string} address
    * @param { string } variableName - variable name within the state
    * @param { string[] } indices - (optional) If the variable is of map type, you can specify an index (or indices)
    * @returns {Promise<RPCResponse<any, string>>}
    */
+
   getSmartContractSubState(
     addr: string,
     variableName: string,
