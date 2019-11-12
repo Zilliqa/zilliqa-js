2b6e78b1a847a574a2b0f372175c8654af74dac9
xiaohuo <xiaohuo200@gmail.com>
2019-11-13 04:29:14 +0800

    chore(zilliqa): add newTxBlockSubscription as Zilliqa's field

--- packages/zilliqa/src/index.ts
+++ packages/zilliqa/src/index.ts
@@ -17,22 +17,24 @@ import { HTTPProvider, Provider } from '@zilliqa-js/core';
 import { TransactionFactory, Wallet } from '@zilliqa-js/account';
 import { Contracts } from '@zilliqa-js/contract';
 import { Blockchain, Network } from '@zilliqa-js/blockchain';
+import { NewTxBlockSubscription } from '@zilliqa-js/subscriptions';
 
 export class Zilliqa {
   provider: Provider;
-
   blockchain: Blockchain;
   network: Network;
   contracts: Contracts;
   transactions: TransactionFactory;
   wallet: Wallet;
+  newTxBlockSubscription: NewTxBlockSubscription;
 
-  constructor(node: string, provider?: Provider) {
+  constructor(node: string, websocket: string, provider?: Provider) {
     this.provider = provider || new HTTPProvider(node);
     this.wallet = new Wallet(this.provider);
     this.blockchain = new Blockchain(this.provider, this.wallet);
     this.network = new Network(this.provider, this.wallet);
     this.contracts = new Contracts(this.provider, this.wallet);
     this.transactions = new TransactionFactory(this.provider, this.wallet);
+    this.newTxBlockSubscription = new NewTxBlockSubscription(websocket);
   }
 }
