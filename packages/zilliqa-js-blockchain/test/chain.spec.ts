6f046b39c7287e29f7072dbb36f9295308649114
xiaohuo <xiaohuo200@gmail.com>
2020-01-21 16:13:07 +0800

    feat(receipt): add one more unit test

--- packages/zilliqa-js-blockchain/test/chain.spec.ts
+++ packages/zilliqa-js-blockchain/test/chain.spec.ts
@@ -128,6 +128,76 @@ describe('Module: Blockchain', () => {
       'The transaction is still not confirmed after 40 attempts.',
     );
   });
+
+  it('should receive transition array', async () => {
+    const tx = new Transaction(
+      {
+        version: 1,
+        toAddr: '0x1234567890123456789012345678901234567890',
+        amount: new BN(0),
+        gasPrice: new BN(1000),
+        gasLimit: Long.fromNumber(1000),
+      },
+      provider,
+    );
+
+    const responses = [
+      {
+        id: 1,
+        jsonrpc: '2.0',
+        result: {
+          balance: 888,
+          nonce: 1,
+        },
+      },
+      {
+        id: 1,
+        jsonrpc: '2.0',
+        result: {
+          TranID: 'some_hash',
+          Info: 'Non-contract txn, sent to shard',
+        },
+      },
+      {
+        id: 1,
+        jsonrpc: '2.0',
+        result: {
+          ID: 'some_hash',
+          receipt: {
+            success: true,
+            transitions: [
+              {
+                addr: '0xde641801b4c3a87298808ffe260ecf9b32bd958f',
+                msg: {
+                  _amount: '0',
+                  _recipient: '0x0dcca194fa156e388f01b3c4b9993d031d596699',
+                  _tag: 'callback',
+                  params: [{ type: 'String', value: '5', vname: 'result' }],
+                },
+              },
+            ],
+          },
+        },
+      },
+    ].map((res) => [JSON.stringify(res)] as [string]);
+
+    fetch.mockResponses(...responses);
+
+    const { txParams } = await blockchain.createTransaction(tx);
+    expect(txParams.receipt).not.toBeNull();
+    expect(txParams.receipt!.transitions).toEqual([
+      {
+        addr: '0xde641801b4c3a87298808ffe260ecf9b32bd958f',
+        msg: {
+          _amount: '0',
+          _recipient: '0x0dcca194fa156e388f01b3c4b9993d031d596699',
+          _tag: 'callback',
+          params: [{ type: 'String', value: '5', vname: 'result' }],
+        },
+      },
+    ]);
+  });
+
   it('should test the maxAttempts parameter using blockConfirm', async () => {
     const responses = [
       {
