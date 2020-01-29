4fb2ea516b24b557e3b781308017ba48d5716b73
xiaohuo <xiaohuo200@gmail.com>
2020-01-29 12:55:17 +0800

    chore: simplify example

--- examples/createTransactionRaw.js
+++ examples/createTransactionRaw.js
@@ -8,24 +8,6 @@ const {
 
 const zilliqa = new Zilliqa('https://dev-api.zilliqa.com');
 
-// These are set by the core protocol, and may vary per-chain.
-// You can manually pack the bytes according to chain id and msg version.
-// For more information: https://apidocs.zilliqa.com/?shell#getnetworkid
-
-const chainId = 333; // chainId of the developer testnet
-const msgVersion = 1; // current msgVersion
-const VERSION = bytes.pack(chainId, msgVersion);
-
-// Populate the wallet with an account
-const privateKey =
-  '3375F915F3F9AE35E6B301B7670F53AD1A5BE15D8221EC7FD5E503F21D3450C8';
-
-zilliqa.wallet.addByPrivateKey(privateKey);
-
-const address = getAddressFromPrivateKey(privateKey);
-console.log(`My account address is: ${address}`);
-console.log(`My account bech32 address is: ${toBech32Address(address)}`);
-
 async function testBlockchain() {
   try {
     const payload =
@@ -41,6 +23,8 @@ async function testBlockchain() {
     // process confirm
     console.log(`The transaction id is:`, id);
     console.log(`Waiting transaction be confirmed`);
+
+    // to perform confirmation, we need a `empty` transaction object
     const emptyTx = new Transaction(
       { toAddr: '0x' + tx.toAddr },
       zilliqa.provider,
