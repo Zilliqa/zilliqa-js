1c1a314753a21ec48ddd583bd985fb366e6cf1ec
Noel <86328181+noel-yoo@users.noreply.github.com>
2021-08-17 20:41:06 +0900

    refactor(blockchain): comments (#351)

--- packages/zilliqa-js-blockchain/src/chain.ts
+++ packages/zilliqa-js-blockchain/src/chain.ts
@@ -112,246 +112,107 @@ export class Blockchain implements ZilliqaModule {
     this.signer = signer;
   }
 
-  /**
-   * getBlockChainInfo
-   *
-   * @returns {Promise<RPCResponse<BlockchainInfo, string>>}
-   */
   getBlockChainInfo(): Promise<RPCResponse<BlockchainInfo, string>> {
     return this.provider.send(RPCMethod.GetBlockchainInfo);
   }
 
-  /**
-   * getShardingStructure
-   *
-   * @returns {Promise<RPCResponse<ShardingStructure, string>>}
-   */
   getShardingStructure(): Promise<RPCResponse<ShardingStructure, string>> {
     return this.provider.send(RPCMethod.GetShardingStructure);
   }
 
-  /**
-   * getDSBlock
-   *
-   * Get details of a Directory Service block by block number.
-   *
-   * @param {number} blockNum
-   * @returns {Promise<RPCResponse<DsBlockObj, string>>}
-   */
+  // Gets details of a Directory Service block by block number.
   getDSBlock(blockNum: number): Promise<RPCResponse<DsBlockObj, string>> {
     return this.provider.send(RPCMethod.GetDSBlock, blockNum.toString());
   }
 
-  /**
-   * getLatestDSBlock
-   *
-   * Get details of the most recent Directory Service block.
-   *
-   * @returns {Promise<RPCResponse<DsBlockObj, string>>}
-   */
+  // Gets details of the most recent Directory Service block.
   getLatestDSBlock(): Promise<RPCResponse<DsBlockObj, string>> {
     return this.provider.send(RPCMethod.GetLatestDSBlock);
   }
 
-  /**
-   * getNumDSBlocks
-   *
-   * Gets the number of DS blocks that the network has processed.
-   *
-   * @returns {Promise<RPCResponse<string, string>>}
-   */
+  // Gets the number of DS blocks that the network has processed.
   getNumDSBlocks(): Promise<RPCResponse<string, string>> {
     return this.provider.send(RPCMethod.GetNumDSBlocks);
   }
 
-  /**
-   * getDSBlockRate
-   *
-   * Gets the average rate of DS blocks processed per second
-   *
-   * @returns {Promise<RPCResponse<number, string>>}
-   */
+  // Gets the average rate of DS blocks processed per second
   getDSBlockRate(): Promise<RPCResponse<number, string>> {
     return this.provider.send(RPCMethod.GetDSBlockRate);
   }
 
-  /**
-   * getDSBlockListing
-   *
-   * Get a paginated list of Directory Service blocks. Pass in page number as
-   * parameter. Returns a maxPages variable that specifies the max number of
-   * pages. 1 - latest blocks, maxPages - oldest blocks.
-   *
-   * @param {number} max
-   * @returns {Promise<RPCResponse<BlockList, string>>}
-   */
+  // Gets a paginated list of up to 10 Directory Service (DS) blocks
+  // and their block hashes for a specified page.
   getDSBlockListing(max: number): Promise<RPCResponse<BlockList, string>> {
     return this.provider.send(RPCMethod.DSBlockListing, max);
   }
 
-  /**
-   * getTxBlock
-   *
-   * Get details of a Transaction block by block number.
-   *
-   * @param {number} blockNum
-   * @returns {Promise<RPCResponse<TxBlockObj, string>>}
-   */
+  // Gets details of a Transaction block by block number.
   getTxBlock(blockNum: number): Promise<RPCResponse<TxBlockObj, string>> {
     return this.provider.send(RPCMethod.GetTxBlock, blockNum.toString());
   }
 
-  /**
-   * getLatestTxBlock
-   *
-   * Get details of the most recent Transaction block.
-   *
-   * @returns {Promise<RPCResponse<TxBlockObj, string>>}
-   */
+  // Gets details of the most recent Transaction block.
   getLatestTxBlock(): Promise<RPCResponse<TxBlockObj, string>> {
     return this.provider.send(RPCMethod.GetLatestTxBlock);
   }
 
-  /**
-   * getNumTxBlocks
-   *
-   * Gets the total number of TxBlocks.
-   *
-   * @returns {Promise<RPCResponse<string, string>>}
-   */
+  // Gets the total number of TxBlocks.
   getNumTxBlocks(): Promise<RPCResponse<string, string>> {
     return this.provider.send(RPCMethod.GetNumTxBlocks);
   }
 
-  /**
-   * getTxBlockRate
-   *
-   * Gets the average number of Tx blocks per second.
-   *
-   * @returns {Promise<RPCResponse<number, string>>}
-   */
+  // Gets the average number of Tx blocks per second.
   getTxBlockRate(): Promise<RPCResponse<number, string>> {
     return this.provider.send(RPCMethod.GetTxBlockRate);
   }
 
-  /**
-   * getTxBlockListing
-   *
-   * Get a paginated list of Transaction blocks. Takes a page number as
-   * parameter, where each page contains a list of 10 blocks (max). Returns
-   * a maxPages variable that specifies the max number of pages. 1 - latest
-   * blocks, maxPages - oldest blocks.
-   *
-   * @param {number} max
-   * @returns {Promise<RPCResponse<BlockList, string>>}
-   */
+  // Get a paginated list of Transaction blocks.
   getTxBlockListing(max: number): Promise<RPCResponse<BlockList, string>> {
     return this.provider.send(RPCMethod.TxBlockListing, max);
   }
 
-  /**
-   * getNumTransactions
-   *
-   * Gets the number of transactions processed by the network so far.
-   *
-   * @returns {Promise<RPCResponse<string, string>>}
-   */
+  // Gets the number of transactions processed by the network so far.
   getNumTransactions(): Promise<RPCResponse<string, string>> {
     return this.provider.send(RPCMethod.GetNumTransactions);
   }
 
-  /**
-   * getTransactionRate
-   *
-   * Gets the number of transactions processed per second
-   *
-   * @returns {Promise<RPCResponse<number, string>>}
-   */
+  // Gets the number of transactions processed per second
   getTransactionRate(): Promise<RPCResponse<number, string>> {
     return this.provider.send(RPCMethod.GetTransactionRate);
   }
 
-  /**
-   * getCurrentMiniEpoch
-   *
-   * Gets the current Tx Epoch.
-   *
-   * @returns {Promise<RPCResponse<string, string>>}
-   */
+  // Gets the current Tx Epoch.
   getCurrentMiniEpoch(): Promise<RPCResponse<string, string>> {
     return this.provider.send(RPCMethod.GetCurrentMiniEpoch);
   }
 
-  /**
-   * getCurrentDSEpoch
-   *
-   * Gets the current DS Epoch.
-   *
-   * @returns {Promise<RPCResponse<any, string>>}
-   */
+  // Gets the current DS Epoch.
   getCurrentDSEpoch(): Promise<RPCResponse<any, string>> {
     return this.provider.send(RPCMethod.GetCurrentDSEpoch);
   }
 
-  /**
-   * getPrevDifficulty
-   *
-   * Gets shard difficulty for previous PoW round
-   *
-   * @returns {Promise<RPCResponse<number, string>>}
-   */
+  // Gets shard difficulty for previous PoW round
   getPrevDifficulty(): Promise<RPCResponse<number, string>> {
     return this.provider.send(RPCMethod.GetPrevDifficulty);
   }
 
-  /**
-   * getPrevDSDifficulty
-   *
-   * Gets DS difficulty for previous PoW round
-   *
-   * @returns {Promise<RPCResponse<number, string>>}
-   */
+  // Gets DS difficulty for previous PoW round
   getPrevDSDifficulty(): Promise<RPCResponse<number, string>> {
     return this.provider.send(RPCMethod.GetPrevDSDifficulty);
   }
 
-  /**
-   * getTotalCoinSupply
-   *
-   * Returns the total supply (ZIL) of coins in the network.
-   */
+  // Returns the total supply (ZIL) of coins in the network.
   getTotalCoinSupply(): Promise<RPCResponse<string, string>> {
     return this.provider.send(RPCMethod.GetTotalCoinSupply);
   }
 
-  /**
-   * getMinerInfo
-   *
-   * Returns the mining nodes (i.e., the members of the DS committee and shards) at the specified DS block.
-   *
-   * Notes:
-   * 1. Nodes owned by Zilliqa Research are omitted.
-   * 2. dscommittee has no size field since the DS committee size is fixed for a given chain.
-   * 3. For the Zilliqa Mainnet, this API is only available from DS block 5500 onwards.
-   *
-   */
+  // Returns the mining nodes (i.e., the members of the DS committee and shards)
+  // at the specified DS block.
   getMinerInfo(dsBlockNumber: string): Promise<RPCResponse<MinerInfo, any>> {
     return this.provider.send(RPCMethod.GetMinerInfo, dsBlockNumber);
   }
 
-  /**
-   * createTransaction
-   *
-   * Creates a transaction and polls the lookup node for a transaction
-   * receipt. The transaction is considered to be lost if it is not confirmed
-   * within the timeout period.
-   *
-   * @param {Transaction} tx
-   * @param {number} maxAttempts - (optional) number of times to poll before timing out
-   * @param {number} number - (optional) interval in ms
-   * @returns {Promise<Transaction>} - the Transaction that has been signed and
-   * broadcasted to the network.
-   */
+  // Creates a transaction and polls the lookup node for a transaction receipt.
   @sign
   async createTransaction(
     tx: Transaction,
@@ -429,16 +290,10 @@ export class Blockchain implements ZilliqaModule {
     }
   }
 
-  /**
-   * createTransactionRaw
-   *
-   * Create a transaction by using a exist signed transaction payload
-   * This payload may come form some offline signing software like ledger
-   * Currently we haven't supported convert a singed transaction back to transaction param, so we won't perform
-   * confirm logic here.
-   *
-   * @param payload
-   */
+  // Create a transaction by using a exist signed transaction payload
+  // This payload may come form some offline signing software like ledger
+  // Currently we haven't supported convert a singed transaction back to transaction param, so we won't perform
+  // confirm logic here.
   async createTransactionRaw(payload: string): Promise<string> {
     try {
       const tx = JSON.parse(payload);
@@ -510,17 +365,7 @@ export class Blockchain implements ZilliqaModule {
     }
   }
 
-  /**
-   * getTransaction
-   *
-   * Retrieves a transaction from the blockchain by its hash. If the result
-   * contains an Error, a rejected Promise is returned with the erorr message.
-   * If it does not contained an error, but `receipt.success` is `false`, then
-   * a rejected Transaction instance is returned.
-   *
-   * @param {string} txHash
-   * @returns {Promise<Transaction>}
-   */
+  // Returns the details of a specified Transaction.
   async getTransaction(txHash: string): Promise<Transaction> {
     try {
       const response = await this.provider.send<TransactionObj>(
@@ -540,14 +385,7 @@ export class Blockchain implements ZilliqaModule {
     }
   }
 
-  /**
-   * Returns the status of a specified transaction.
-   * This API is available from Zilliqa `V7.0.0` onwards and supports all transaction statuses
-   * (unconfirmed, confirmed, and rejected).
-   *
-   * @param txHash
-   * @returns {Promise<TransactionStatusObj>}
-   */
+  // Returns the status of a specified transaction.
   async getTransactionStatus(txHash: string): Promise<TransactionStatusObj> {
     try {
       const response = await this.provider.send<TransactionStatusObj>(
@@ -568,25 +406,14 @@ export class Blockchain implements ZilliqaModule {
     }
   }
 
-  /**
-   * getRecentTransactions
-   *
-   * Gets a list of recent transactions
-   *
-   * @returns {Promise<RPCResponse<TxList, never>>}
-   */
+  // Gets a list of recent transactions
   getRecentTransactions(): Promise<RPCResponse<TxList, never>> {
     return this.provider.send(RPCMethod.GetRecentTransactions);
   }
 
-  /**
-   * getTransactionsForTxBlock
-   *
-   * Gets all transactions for a given TxBlock, grouped by shard id
-   *
-   * @param {number} txBlock
-   * @returns {Promise<RPCResponse<string[][], string>>}
-   */
+  // Returns the validated transactions included
+  // within a specified final transaction block as an array of
+  // length i, where i is the number of shards plus the DS committee.
   getTransactionsForTxBlock(
     txBlock: number,
   ): Promise<RPCResponse<string[][], string>> {
@@ -596,12 +423,8 @@ export class Blockchain implements ZilliqaModule {
     );
   }
 
-  /**
-   * getTxnBodiesForTxBlock
-   *
-   * @param {number} txBlock
-   * @returns { romise<RPCResponse<TransactionObj[], string>>}
-   */
+  // Returns the validated transactions (in verbose form)
+  // included within a specified final transaction block.
   getTxnBodiesForTxBlock(
     txBlock: number,
   ): Promise<RPCResponse<TransactionObj[], string>> {
@@ -611,49 +434,22 @@ export class Blockchain implements ZilliqaModule {
     );
   }
 
-  /**
-   * getNumTxnsTxEpoch
-   *
-   * Gets the number of transactions procesed for a given Tx Epoch.
-   *
-   * @param {number} epoch
-   * @returns {Promise<RPCResponse<number, never>>}
-   */
+  // Gets the number of transactions procesed for a given Tx Epoch.
   getNumTxnsTxEpoch(epoch: number): Promise<RPCResponse<string, string>> {
     return this.provider.send(RPCMethod.GetNumTxnsTxEpoch, epoch);
   }
 
-  /**
-   * getNumTxnsDSEpoch
-   *
-   * Gets the number of transactions procesed for a given DS Epoch.
-   *
-   * @param {number} epoch
-   * @returns {Promise<any>}
-   */
+  // Gets the number of transactions procesed for a given DS Epoch.
   getNumTxnsDSEpoch(epoch: number): Promise<RPCResponse<string, string>> {
     return this.provider.send(RPCMethod.GetNumTxnsDSEpoch, epoch);
   }
 
-  /**
-   * getMinimumGasPrice
-   *
-   * Gets the numeric minimum gas price
-   *
-   * @returns {Promise<RPCResponse<string, string>>}
-   */
+  // Gets the numeric minimum gas price.
   getMinimumGasPrice() {
     return this.provider.send<string, string>(RPCMethod.GetMinimumGasPrice);
   }
 
-  /**
-   * getBalance
-   *
-   * Gets the balance of an account by address
-   *
-   * @param {string} address
-   * @returns {Promise<RPCResponse<any, string>>}
-   */
+  // Gets the balance of an account by address.
   getBalance(addr: string): Promise<RPCResponse<any, string>> {
     const address = validation.isBech32(addr) ? fromBech32Address(addr) : addr;
     return this.provider.send(
@@ -662,12 +458,7 @@ export class Blockchain implements ZilliqaModule {
     );
   }
 
-  /**
-   * getSmartContractCode - returns the smart contract code of a deployed contract.
-   *
-   * @param {string} address
-   * @returns {Promise<RPCResponse<{code: string }, string>>}
-   */
+  // Returns the Scilla code associated with a smart contract address
   getSmartContractCode(
     addr: string,
   ): Promise<RPCResponse<{ code: string }, string>> {
@@ -678,12 +469,8 @@ export class Blockchain implements ZilliqaModule {
     );
   }
 
-  /**
-   * getSmartContractInit
-   *
-   * @param {string} address
-   * @returns {Promise<RPCResponse<Value[], string>>}
-   */
+  // Returns the initialization (immutable) parameters of
+  // a given smart contract, represented in a JSON format.
   getSmartContractInit(addr: string): Promise<RPCResponse<Value[], string>> {
     const address = validation.isBech32(addr) ? fromBech32Address(addr) : addr;
     return this.provider.send(
@@ -692,12 +479,7 @@ export class Blockchain implements ZilliqaModule {
     );
   }
 
-  /**
-   * getSmartContractState - retrieves the entire state of a smart contract
-   *
-   * @param {string} address
-   * @returns {Promise<RPCResponse<any, string>>}
-   */
+  // Retrieves the entire state of a smart contract.
   getSmartContractState(addr: string): Promise<RPCResponse<any, string>> {
     const address = validation.isBech32(addr) ? fromBech32Address(addr) : addr;
     return this.provider.send(
@@ -706,18 +488,7 @@ export class Blockchain implements ZilliqaModule {
     );
   }
 
-  /**
-   * getSmartContractSubState - Queries the contract state, filtered by the variable names.
-   * This function is the filtered version of `getSmartContractState`.
-   * As `getSubState` performs the filtering, `variableName` of a field is required.
-   * If the `subState` is not found, this returns a `null` response.
-   *
-   * @param {string} address
-   * @param { string } variableName - variable name within the state
-   * @param { string[] } indices - (optional) If the variable is of map type, you can specify an index (or indices)
-   * @returns {Promise<RPCResponse<any, string>>}
-   */
-
+  // Queries the contract state, filtered by the variable names.
   getSmartContractSubState(
     addr: string,
     variableName: string,
@@ -736,22 +507,11 @@ export class Blockchain implements ZilliqaModule {
     );
   }
 
-  /**
-   * getSmartContractSubStateBatch - Quires the contract state using batch rpc.
-   * @param reqs array of address variableName indices
-   *  e.g ["5938fc8af82250ad6cf1da3bb92f4aa005cb2717","balances",['0x381f4008505e940ad7681ec3468a719060caf796']]
-   * @returns
-   */
+  // Queries the contract state using batch rpc.
   getSmartContractSubStateBatch(reqs: any[]): Promise<RPCResponse<any, any>> {
     return this.provider.sendBatch(RPCMethod.GetSmartContractSubState, reqs);
   }
 
-  /**
-   * getSmartContracts
-   *
-   * @param {string} address
-   * @returns {Promise<RPCResponse<ContractObj[], string>>}
-   */
   getSmartContracts(addr: string): Promise<RPCResponse<ContractObj[], string>> {
     const address = validation.isBech32(addr) ? fromBech32Address(addr) : addr;
     return this.provider.send(
@@ -760,12 +520,6 @@ export class Blockchain implements ZilliqaModule {
     );
   }
 
-  /**
-   * getContractAddressFromTransactionID
-   *
-   * @param {string} txHash
-   * @returns {Promise<RPCResponse<string, string>>}
-   */
   getContractAddressFromTransactionID(
     txHash: string,
   ): Promise<RPCResponse<string, string>> {
